5deeb9d984364381af530738ca5c0953c0e77479
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-07-25 15:25:48 +0100

    feat: add post-hook to initialise a git repo

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Post-generation tasks
+
+File: hooks/post_gen_project.py
+
+Copyright 2025 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+import os
+import sys
+import subprocess
+
+
+def use_git():
+    """Commit initial project to a git repository
+    :returns: TODO
+
+    """
+    print("Initialising a new Git repository")
+    subprocess.run(["git", "init"], check=True)
+
+    print("Adding files")
+    subprocess.run(["git", "add", "."], check=True)
+
+    print("Making initial commit")
+    subprocess.run(["git", "commit", "-m", "Init"], check=True)
+
+    print("Git repository initialized")
+    return 0
+
+
+
+if __name__ == "__main__":
+    {% if cookiecutter.use_git|lower != "n" %}
+
+    sys.exit(use_git())
+
+    {% else %}
+
+    sys.exit(0)
+
+    {% endif %}
